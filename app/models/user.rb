6cb223875c484ebceb125250480de0d9e76b4e3f
GustavoCig <gustavo.cignachi@gocase.com.br>
2019-06-10 01:23:42 -0300

    Add methods in User model to manage user's decks

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def decks
+    Deck.where(user_id: id)
+  end
+
+  def create_deck(cards)
+    Deck.create(cards: cards, user_id: id)
+  end
+
+  def get_deck(deck_id)
+    Deck.find_by(id: deck_id, user_id: id)
+  end
 end
