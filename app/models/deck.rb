2070b6f01d9c5cc2ff8b2ae5f09c0130281a8928
GustavoCig <gustavo.cignachi@gocase.com.br>
2019-06-18 23:30:20 -0300

    Add tentative active_storage attributes to Deck(card_arts) and User(avatar)

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,6 +1,8 @@
 class Deck
   include Mongoid::Document
 
+  has_many_attached :card_arts
+
   field :user_id, type: String
   field :cards, type: Array
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+
+  validates :name, presence: true, uniqueness: true
+
   def decks
     Deck.where(user_id: id)
   end
