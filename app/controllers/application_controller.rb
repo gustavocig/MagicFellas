e7d51abf7e7ef53a724b6b9cfaf0cf7a8b3bf975
GustavoCig <gustavo.cignachi@gocase.com.br>
2019-06-23 17:13:49 -0300

    Add #name to User Sign Up view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_devise_params, if: :devise_controller?
+
+  protected
+
+  def configure_devise_params
+    devise_parameter_sanitizer.permit :sign_up, keys: [:name, :email, :password, :password_confirmation]
+  end
 end
