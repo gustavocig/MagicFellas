86de077f097bf375409c44359b9c7dbeb68fd08c
GustavoCig <gustavo.cignachi@gocase.com.br>
2019-06-23 15:46:01 -0300

    Set index card loading

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,32 @@
+class CardsController < ApplicationController
+  def index
+    @current_page = params[:page]&.to_i || 1
+    
+    @cards = Requester.cards({page: @current_page})
+
+    @has_more_pages = @cards['has_more']
+    @total_cards = @cards['total_cards']
+    @cards_per_page = @cards['data'].size
+
+    format_cards! @cards['data']
+    set_pagination
+
+    render 'user/index'
+  end
+
+  def search
+  end
+
+  def format_cards!(cards)
+    cards.each do |card|
+      Requester.formatted_request! card
+    end
+  end
+
+  def set_pagination
+    @first_page = @current_page > 2 ? 1 : nil
+    @final_page = @has_more_pages ? (@total_cards / @cards_per_page.to_f).ceil : nil
+    @next_page = (@has_more_pages && @current_page + 1 != @final_page) ? @current_page + 1 : nil
+    @previous_page = @current_page == 1 ? nil : @current_page - 1
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: 'user#index'
-  get '/search', to: 'search#index', as: :search
+  get '/search', to: 'cards#search', as: :cards_search
+  get '/cards', to: 'cards#index', as: :cards_index
 
   namespace :api do
   	namespace :v1 do
