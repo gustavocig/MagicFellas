b8bdab67f49f7d80a8e2fa85584b4ee7606b686d
GustavoCig <gustavo.cignachi@gocase.com.br>
2019-06-24 14:26:28 -0300

    Fix garbage deck

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -6,31 +6,5 @@ class Deck
   field :cards, type: Array
   field :name, type: String
 
-  validate :unique_deck_name_by_user
-
-  before_validation :set_name
-
-  def set_name
-    return unless name.nil?
-
-    n = 1
-    loop do
-      self.name = "Deck #{n}"
-
-      break if save
-
-      n += 1
-    end
-  end
-
-  private
-
-  def unique_deck_name_by_user
-    user = User.find user_id
-    user_deck_names = user.decks.select { |deck| deck.id != id }.pluck(:name)
-
-    return unless user_deck_names.include? name
-
-    errors.add(:name, 'already in use by user')
-  end
+  validates :name, presence: true, uniqueness: true
 end
