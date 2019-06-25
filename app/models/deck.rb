a3d27d4660e4be10241ba9d7deadc456052685dd
GustavoCig <gustavo.cignachi@gocase.com.br>
2019-06-25 07:19:08 -0300

    Add valid_deck field to decks alongside callback that sets it's value

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,10 +1,19 @@
 class Deck
   include Mongoid::Document
   IMAGE_RATIO = { width: '280', height: '395' }.freeze
+  MIN_VALID_DECK_SIZE = 60.freeze
 
   field :user_id, type: Integer
   field :cards, type: Array
   field :name, type: String
+  field :valid_deck, type: Boolean
 
   validates :name, presence: true, uniqueness: true
+
+  before_create :set_valid_deck
+  before_save :set_valid_deck
+
+  def set_valid_deck
+    self.valid_deck = (cards.count >= MIN_VALID_DECK_SIZE)
+  end
 end
